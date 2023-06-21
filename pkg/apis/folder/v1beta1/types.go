c457644aef1ffe8a37cb209ced74758c19639412
Yuzhou Liu <yuzhou.liu@mongodb.com>
2023-06-21 14:13:47 -0400

    Add Folder XRD

---
+++ pkg/apis/folder/v1beta1/types.go
@@ -0,0 +1,36 @@
+// +groupName=grafana.kanopy-platform.github.io
+// +versionName=v1beta1
+// +kubebuilder:validation:Optional
+package v1beta1
+
+import (
+	xpapiext "github.com/crossplane/crossplane/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Grafana Folder resource.
+// +kubebuilder:object:root=true
+// +kubebuilder:printcolumn:name="ESTABLISHED",type="string",JSONPath=".status.conditions[?(@.type=='Established')].status"
+// +kubebuilder:printcolumn:name="OFFERED",type="string",JSONPath=".status.conditions[?(@.type=='Offered')].status"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:storageversion
+// +kubebuilder:resource:scope=Cluster,categories=crossplane,shortName=xrd;xrds
+// +kubebuilder:defaultcompositionref:name=provisionedfolders,enforced=true
+type ProvisionedFolder struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ProvisionedFolderSpec                      `json:"spec,omitempty"`
+	Status xpapiext.CompositeResourceDefinitionStatus `json:"status,omitempty"`
+}
+
+type ProvisionedFolderSpec struct {
+	// +kubebuilder:validation:Required
+	FolderTitle string `json:"foldertitle,omitempty"`
+	// +kubebuilder:validation:Optional
+	FolderUID string `json:"folderuid,omitempty"`
+	// +kubebuilder:validation:Optional
+	DeletionPolicy string `json:"deletionpolicy,omitempty"`
+	// +kubebuilder:validation:Required
+	Namespace string `json:"namespace,omitempty"`
+}
